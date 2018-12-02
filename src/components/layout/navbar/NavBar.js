42cc0dc59032c60524b0bebde369a7cd686ae14b
odm275 <pomejia@gmail.com>
2018-12-01 18:44:08 -0600

    typography....

--- src/components/layout/navbar/NavBar.js
+++ src/components/layout/navbar/NavBar.js
@@ -118,7 +118,8 @@ class NavBar extends Component {
                 <MenuIcon />
               </IconButton>
               <Typography
-                variant="h6"
+                component="h1"
+                variant="headline"
                 color="inherit"
                 noWrap
                 className={classes.title}
